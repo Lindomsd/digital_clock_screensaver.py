4bd2562473786bd38c2a82a153e2244dc172cf56
Linda Dlamini <39090508+Lindomsd@users.noreply.github.com>
2023-11-22 21:55:23 +0200

    Update digital_clock_screensaver.py
    added date to the initial code

--- digital_clock_screensaver.py
+++ digital_clock_screensaver.py
@@ -2,23 +2,24 @@ import tkinter as tk
 import time
 import ctypes
 
-def display_time():
+def display_time_and_date():
     current_time = time.strftime('%H:%M:%S')
-    clock_label.config(text=current_time)
-    clock_label.after(1000, display_time)
+    current_date = time.strftime('%A, %B %d, %Y')
+    clock_label.config(text=current_time + "\n" + current_date)
+    clock_label.after(1000, display_time_and_date)
 
 root = tk.Tk()
 root.title("Digital Clock Screensaver")
-root.geometry("250x150")
+root.geometry("350x200")
 
 # Get system color for the window background
 color = ctypes.windll.user32.GetSysColor(1)  # 1 for the default color of the window
 
 root.configure(bg='#{:06x}'.format(color))  # Set window background color
 
-clock_label = tk.Label(root, font=('Arial', 60), fg='white', bg='#{:06x}'.format(color))
-clock_label.pack(pady=40)
+clock_label = tk.Label(root, font=('Arial', 40), fg='white', bg='#{:06x}'.format(color))
+clock_label.pack(pady=60)
 
-display_time()
+display_time_and_date()
 
 root.mainloop()
